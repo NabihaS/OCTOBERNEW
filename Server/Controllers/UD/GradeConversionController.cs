7e02d5fff82b6427e38d296deb279eea2714528d
NabihaS <nazasy@udel.edu>
2023-11-14 01:39:51 -0500

    grade conversion methods

--- Server/Controllers/UD/GradeConversionController.cs
+++ Server/Controllers/UD/GradeConversionController.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Caching.Memory;
 using OCTOBER.EF.Data;
+using OCTOBER.EF.Models;
 using OCTOBER.Server.Controllers.Base;
 using OCTOBER.Shared.DTO;
 
@@ -19,29 +22,179 @@ namespace OCTOBER.Server.Controllers.UD
         {
         }
 
-        public Task<IActionResult> Delete(int KeyVal)
+        [HttpDelete]
+        [Route("Delete/{SchoolId}/{LetterGrade}")]
+        public async Task<IActionResult> Delete(int SchoolId, string LetterGrade)
         {
-            throw new NotImplementedException();
+
+            Debugger.Launch();
+
+            try
+            {
+                await _context.Database.BeginTransactionAsync();
+
+                var itm = await _context.GradeConversions
+                    .Where(x => x.SchoolId == SchoolId)
+                    .Where(x => x.LetterGrade == LetterGrade).FirstOrDefaultAsync();
+
+                if (itm != null)
+                {
+                    _context.GradeConversions.Remove(itm);
+                }
+                await _context.SaveChangesAsync();
+                await _context.Database.CommitTransactionAsync();
+
+                return Ok();
+            }
+            catch (Exception Dex)
+            {
+                await _context.Database.RollbackTransactionAsync();
+                //List<OraError> DBErrors = ErrorHandling.TryDecodeDbUpdateException(Dex, _OraTranslateMsgs);
+                return StatusCode(StatusCodes.Status417ExpectationFailed, "An Error has occurred");
+            }
         }
 
-        public Task<IActionResult> Get()
+        [HttpGet]
+        [Route("Get")]
+
+        public async Task<IActionResult> Get()
         {
-            throw new NotImplementedException();
+            try
+            {
+                await _context.Database.BeginTransactionAsync();
+
+                var result = await _context
+                    .GradeConversions
+                    .Select(sp => new GradeConversionDTO
+                    {
+                        SchoolId = sp.SchoolId,
+                        LetterGrade = sp.LetterGrade,
+                        MaxGrade = sp.MaxGrade,
+                        MinGrade = sp.MinGrade,
+                        GradePoint = sp.GradePoint,
+                        CreatedBy = sp.CreatedBy,
+                        CreatedDate = sp.CreatedDate,
+                        ModifiedBy = sp.ModifiedBy,
+                        ModifiedDate = sp.ModifiedDate,
+                    })
+                .SingleOrDefaultAsync();
+
+                await _context.Database.RollbackTransactionAsync();
+                return Ok(result);
+            }
+            catch (Exception Dex)
+            {
+                await _context.Database.RollbackTransactionAsync();
+                //List<OraError> DBErrors = ErrorHandling.TryDecodeDbUpdateException(Dex, _OraTranslateMsgs);
+                return StatusCode(StatusCodes.Status417ExpectationFailed, "An Error has occurred");
+            }
         }
 
-        public Task<IActionResult> Get(int KeyVal)
+        [HttpGet]
+        [Route("Get/{SchoolID}/{LetterGrade}")]
+
+        public async Task<IActionResult> Get(int SchoolId, string LetterGrade)
         {
-            throw new NotImplementedException();
+            try
+            {
+                await _context.Database.BeginTransactionAsync();
+
+                GradeConversionDTO? result = await _context
+                    .GradeConversions
+                    .Where(x => x.SchoolId == SchoolId)
+                    .Where(x => x.LetterGrade == LetterGrade)
+                    .Select(sp => new GradeConversionDTO
+                     {
+                        SchoolId=sp.SchoolId,
+                        LetterGrade=sp.LetterGrade,
+                        MaxGrade=sp.MaxGrade,
+                        MinGrade=sp.MinGrade,
+                        GradePoint=sp.GradePoint,
+                         CreatedBy = sp.CreatedBy,
+                         CreatedDate = sp.CreatedDate,
+                         ModifiedBy = sp.ModifiedBy,
+                         ModifiedDate = sp.ModifiedDate,
+                     })
+                .SingleOrDefaultAsync();
+
+                await _context.Database.RollbackTransactionAsync();
+                return Ok(result);
+            }
+            catch (Exception Dex)
+            {
+                await _context.Database.RollbackTransactionAsync();
+                //List<OraError> DBErrors = ErrorHandling.TryDecodeDbUpdateException(Dex, _OraTranslateMsgs);
+                return StatusCode(StatusCodes.Status417ExpectationFailed, "An Error has occurred");
+            }
         }
 
-        public Task<IActionResult> Post([FromBody] GradeConversionDTO _T)
+        [HttpPost]
+        [Route("Post")]
+        public async Task<IActionResult> Post([FromBody]
+                                                GradeConversionDTO _gradeConversionDTO)
         {
-            throw new NotImplementedException();
+            try
+            {
+                await _context.Database.BeginTransactionAsync();
+
+                var itm = await _context.GradeConversions.Where(x => x.SchoolId == _gradeConversionDTO.SchoolId).FirstOrDefaultAsync();
+
+                if (itm == null)
+                {
+                    GradeConversionDTO c = new GradeConversionDTO
+                    {
+                        SchoolId = _gradeConversionDTO.SchoolId,
+                        LetterGrade = _gradeConversionDTO.LetterGrade,
+                        MaxGrade = _gradeConversionDTO.MaxGrade,
+                        MinGrade = _gradeConversionDTO.MinGrade,
+                        GradePoint = _gradeConversionDTO.GradePoint,
+                    };
+                    _context.GradeConversions.Add(c);
+                    await _context.SaveChangesAsync();
+                    await _context.Database.CommitTransactionAsync();
+                }
+                return Ok();
+            }
+            catch (Exception Dex)
+            {
+                await _context.Database.RollbackTransactionAsync();
+                //List<OraError> DBErrors = ErrorHandling.TryDecodeDbUpdateException(Dex, _OraTranslateMsgs);
+                return StatusCode(StatusCodes.Status417ExpectationFailed, "An Error has occurred");
+            }
         }
 
-        public Task<IActionResult> Put([FromBody] GradeConversionDTO _T)
+        [HttpPut]
+        [Route("Put")]
+        public async Task<IActionResult> Put([FromBody]
+                                                GradeConversionDTO _gradeConversionDTO)
         {
-            throw new NotImplementedException();
+
+            Debugger.Launch();
+
+            try
+            {
+                await _context.Database.BeginTransactionAsync();
+
+                var itm = await _context.GradeConversions.Where(x => x.SchoolId == _gradeConversionDTO.SchoolId).FirstOrDefaultAsync();
+
+                itm.SchoolId = _gradeConversionDTO.SchoolId;
+                itm.LetterGrade = _gradeConversionDTO.LetterGrade;
+                itm.MaxGrade = _gradeConversionDTO.MaxGrade;
+                itm.MinGrade = _gradeConversionDTO.MinGrade;
+                itm.GradePoint = _gradeConversionDTO.GradePoint;
+
+                _context.GradeConversions.Update(itm);
+                await _context.SaveChangesAsync();
+                await _context.Database.CommitTransactionAsync();
+
+                return Ok();
+            }
+            catch (Exception Dex)
+            {
+                await _context.Database.RollbackTransactionAsync();
+                //List<OraError> DBErrors = ErrorHandling.TryDecodeDbUpdateException(Dex, _OraTranslateMsgs);
+                return StatusCode(StatusCodes.Status417ExpectationFailed, "An Error has occurred");
+            }
         }
     }
 }
